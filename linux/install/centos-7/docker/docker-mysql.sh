19deeeca53237a0308cbaecb3e4936505cacee2f
叶云轩 <marquis_xuan@163.com>
2020-08-28 00:56:02 +0800

    修改脚本输出样式

--- linux/install/centos-7/docker/docker-mysql.sh
+++ linux/install/centos-7/docker/docker-mysql.sh
@@ -20,37 +20,46 @@ MYSQL_PASSWORD=Docker-mysql-123456
 #  暴露端口
 MYSQL_PORT=33060
 
-# 检查是否有 docker 镜像
+# 检查是否有镜像
+echo -e "\033[32m 检查是否有镜像: $DOCKER_IMAGES \033[0m"
+
 if [ -z "$DOCKER_IMAGES" ]; then
-    echo -e "\033[33m  拉取 $DOCKER_CONTAINER_NAME:$DOCKER_CONTAINER_VERSION 镜像 \033[0m"
+    echo -e "\033[32m  拉取 $DOCKER_CONTAINER_NAME:$DOCKER_CONTAINER_VERSION 镜像 \033[0m"
     docker pull $DOCKER_CONTAINER_NAME:$DOCKER_CONTAINER_VERSION
     DOCKER_IMAGES=$DOCKER_CONTAINER_NAME:$DOCKER_CONTAINER_VERSION
 fi
 
 if [ -z "$DOCKER_IMAGES" ]; then
-    echo -e "\033[33m  $DOCKER_CONTAINER_NAME:$DOCKER_CONTAINER_VERSION 镜像拉取失败,程序即将退出 \033[0m"
+    echo -e "\033[32m  $DOCKER_CONTAINER_NAME:$DOCKER_CONTAINER_VERSION 镜像拉取失败,程序即将退出 \033[0m"
     exit
 fi
 
 # 判断是否有同名容器
 CHECK_CONTAINER_NAME=$(docker ps --format='{{.Names}}' | grep $DOCKER_CONTAINER_NAME)
+echo -e "\033[32m 判断是否有同名容器: $CHECK_CONTAINER_NAME \033[0m"
 
 if [ -n "$CHECK_CONTAINER_NAME" ]; then
-    echo -e "\033[33m 已存在 $DOCKER_CONTAINER_NAME:$DOCKER_CONTAINER_VERSION 容器 \033[0m"
+    echo -e "\033[32m 已存在 $DOCKER_CONTAINER_NAME:$DOCKER_CONTAINER_VERSION 容器 \033[0m"
     exit
 fi
 
 # 判断是否有工作空间
+echo -e "\033[32m 判断是否有工作空间: $DOCKER_WROKSPACE \033[0m"
 if [ -d "$DOCKER_WROKSPACE" ]; then
+    echo -e "\033[32m 创建工作空间 \033[0m"
     mkdir -p $MYSQL_DATA $MYSQL_CONF $MYSQL_LOGS
 fi
 
 # 判断是否有默认配置文件
+echo -e "\033[32m 判断是否有默认配置文件: $MYSQL_CONF/my.conf \033[0m"
 if [ -f "$MYSQL_CONF/my.cnf" ]; then
+    echo -e "\033[32m 初始化默认配置文件 \033[0m"
     echo "W215c3FsXQpkZWZhdWx0LWNoYXJhY3Rlci1zZXQgPSB1dGY4bWI0CgpbbXlzcWxfc2FmZV0KZGVmYXVsdC1jaGFyYWN0ZXItc2V0ID0gdXRmOG1iNAoKW2NsaWVudF0KZGVmYXVsdC1jaGFyYWN0ZXItc2V0ID0gdXRmOG1iNAoKW215c3FsZF0KcGlkLWZpbGU9L3Zhci9ydW4vbXlzcWxkL215c3FsZC5waWQKc29ja2V0PS92YXIvcnVuL215c3FsZC9teXNxbGQuc29jawpkYXRhZGlyPS92YXIvbGliL215c3FsCnN5bWJvbGljLWxpbmtzPTAKaW5pdF9jb25uZWN0PSdTRVQgTkFNRVMgdXRmOG1iNCcKY2hhcmFjdGVyLXNldC1zZXJ2ZXI9dXRmOG1iNApjb2xsYXRpb24tc2VydmVyPXV0ZjhtYjRfdW5pY29kZV9jaQpkZWZhdWx0X2F1dGhlbnRpY2F0aW9uX3BsdWdpbj1teXNxbF9uYXRpdmVfcGFzc3dvcmQKY2hhcmFjdGVyLXNldC1zZXJ2ZXI9dXRmOG1iNAo=" | base64 -d >$MYSQL_CONF/my.cnf
 fi
 
-# 启动容器
+echo "创建容器 $DOCKER_CONTAINER_NAME \033[0m"
+
+# 创建容器
 docker run \
     --network $DOCKER_NETWORK_BRIDGE_NAME \
     -v $MYSQL_LOGS:/logs -v /etc/localtime:/etc/localtime:ro \
--- linux/install/centos-7/docker/docker-nginx.sh
+++ linux/install/centos-7/docker/docker-nginx.sh
@@ -19,7 +19,7 @@ NGINX_WORKSPACE_WEB=$DOCKER_WROKSPACE/www
 NGINX_WORKSPACE_LOGS=$DOCKER_WROKSPACE/logs
 
 # 检查是否有镜像
-echo "检查是否有镜像: $DOCKER_IMAGES"
+echo -e "\033[33m 检查是否有镜像: $DOCKER_IMAGES \033[0m"
 if [ -z "$DOCKER_IMAGES" ]; then
     echo -e "\033[33m  拉取 $DOCKER_CONTAINER_NAME:$DOCKER_CONTAINER_VERSION 镜像 \033[0m"
     docker pull $DOCKER_CONTAINER_NAME:$DOCKER_CONTAINER_VERSION
@@ -31,26 +31,26 @@ if [ -z "$DOCKER_IMAGES" ]; then
 fi
 
 # 判断是否有工作空间
-echo "判断是否有工作空间: $DOCKER_WROKSPACE"
+echo -e "\033[33m 判断是否有工作空间: $DOCKER_WROKSPACE \033[0m"
 if [ ! -d "$DOCKER_WROKSPACE" ]; then
-    echo "创建工作空间"
+    echo -e "\033[33m 创建工作空间 \033[0m"
     mkdir -p $NGINX_WORKSPACE_CONF $NGINX_WORKSPACE_FILES $NGINX_WORKSPACE_WEB $NGINX_WORKSPACE_LOGS
 fi
 
 # 判断是否有默认配置文件
-echo "判断是否有默认配置文件: $NGINX_WORKSPACE_CONF/nginx.conf"
+echo -e "\033[33m 判断是否有默认配置文件: $NGINX_WORKSPACE_CONF/nginx.conf \033[0m"
 if [ ! -f "$NGINX_WORKSPACE_CONF/nginx.conf" ]; then
-    echo "初始化默认配置文件"
+    echo -e "\033[33m 初始化默认配置文件 \033[0m"
     # 写配置文件,默认的
-    echo "IyDkuIDoiKzkuIDkuKrov5vnqIvotrPlpJ/kuoYs5aaC5p6c5pyJU1NM44CBZ3ppcOi/meS6m+avlOi+g+a2iOiAl0NQVeeahOW3peS9nO+8jOiAjOS4lOaYr+WkmuaguENQVeeahOivne+8jOWPr+S7peiuvuS4uuWSjENQVeeahOaVsOmHj+S4gOagtwojIHdvcmtlcl9wcm9jZXNzZXMgNDsKCmV2ZW50cyB7Cgl1c2UgZXBvbGw7Cgl3b3JrZXJfY29ubmVjdGlvbnMgMTAyNDsKCWFjY2VwdF9tdXRleCBvbjsKCW11bHRpX2FjY2VwdCBvbjsKfQoKaHR0cCB7CglzZXJ2ZXJfdG9rZW5zIG9mZjsKCWluY2x1ZGUgbWltZS50eXBlczsKCWRlZmF1bHRfdHlwZSBhcHBsaWNhdGlvbi9vY3RldC1zdHJlYW07CglhY2Nlc3NfbG9nIC92YXIvbG9nL25naW54L2FjY2Vzcy5sb2c7CgoJY2xpZW50X21heF9ib2R5X3NpemUgMTAyNE07CglzZW5kZmlsZSBvbjsKCgljbGllbnRfaGVhZGVyX2J1ZmZlcl9zaXplIDIwazsKCWxhcmdlX2NsaWVudF9oZWFkZXJfYnVmZmVycyA4IDQwazsKCWluY2x1ZGUgL2V0Yy9uZ2lueC9zZXJ2ZXJzLyouY29uZjsKfQo=" >$NGINX_WORKSPACE_CONF/nginx.conf
+    echo "IyDkuIDoiKzkuIDkuKrov5vnqIvotrPlpJ/kuoYs5aaC5p6c5pyJU1NM44CBZ3ppcOi/meS6m+avlOi+g+a2iOiAl0NQVeeahOW3peS9nO+8jOiAjOS4lOaYr+WkmuaguENQVeeahOivne+8jOWPr+S7peiuvuS4uuWSjENQVeeahOaVsOmHj+S4gOagtwojIHdvcmtlcl9wcm9jZXNzZXMgNDsKCmV2ZW50cyB7Cgl1c2UgZXBvbGw7Cgl3b3JrZXJfY29ubmVjdGlvbnMgMTAyNDsKCWFjY2VwdF9tdXRleCBvbjsKCW11bHRpX2FjY2VwdCBvbjsKfQoKaHR0cCB7CglzZXJ2ZXJfdG9rZW5zIG9mZjsKCWluY2x1ZGUgbWltZS50eXBlczsKCWRlZmF1bHRfdHlwZSBhcHBsaWNhdGlvbi9vY3RldC1zdHJlYW07CglhY2Nlc3NfbG9nIC92YXIvbG9nL25naW54L2FjY2Vzcy5sb2c7CgoJY2xpZW50X21heF9ib2R5X3NpemUgMTAyNE07CglzZW5kZmlsZSBvbjsKCgljbGllbnRfaGVhZGVyX2J1ZmZlcl9zaXplIDIwazsKCWxhcmdlX2NsaWVudF9oZWFkZXJfYnVmZmVycyA4IDQwazsKCWluY2x1ZGUgL2V0Yy9uZ2lueC9zZXJ2ZXJzLyouY29uZjsKfQo=" | base64 -d >$NGINX_WORKSPACE_CONF/nginx.conf
 fi
 
 # 判断是否有同名容器
 CHECK_CONTAINER_NAME=$(docker ps --format='{{.Names}}' | grep $DOCKER_CONTAINER_NAME)
-echo "判断是否有同名容器: $CHECK_CONTAINER_NAME"
+echo -e "\033[33m 判断是否有同名容器: $CHECK_CONTAINER_NAME \033[0m"
 
 if [ -z "$CHECK_CONTAINER_NAME" ]; then
-    echo "创建容器 $DOCKER_CONTAINER_NAME"
+    echo "创建容器 $DOCKER_CONTAINER_NAME \033[0m"
     # 创建容器
     docker run \
         --name $DOCKER_CONTAINER_NAME \
@@ -62,5 +62,5 @@ if [ -z "$CHECK_CONTAINER_NAME" ]; then
         -v $NGINX_WORKSPACE_WEB:/etc/nginx/www \
         -v $NGINX_WORKSPACE_LOGS:/etc/nginx/logs \
         -v /etc/localtime:/etc/localtime:ro \
-        --restart=always -d $DOCKER_IMAGES
+        --restart=always -d $DOCKER_CONTAINER_NAME:$DOCKER_CONTAINER_VERSION
 fi
