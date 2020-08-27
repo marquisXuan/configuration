f1a3497b07957ebfc936aac0c6045753fd8bb6df
叶云轩 <marquis_xuan@163.com>
2020-08-28 00:48:59 +0800

    type(影响范围): 提交描述
    详细说明
    
    <footer>
        feature(新特性，需求),
        fix(修改问题),
        docs(文档修改),
        style(代码格式修改, 注意不是 css 修改),
        refactor(代码重构),
        test(测试),
        chore(其他修改, 比如构建流程, 依赖管理)

--- linux/install/centos-7/docker/docker-nginx.sh
+++ linux/install/centos-7/docker/docker-nginx.sh
@@ -19,6 +19,7 @@ NGINX_WORKSPACE_WEB=$DOCKER_WROKSPACE/www
 NGINX_WORKSPACE_LOGS=$DOCKER_WROKSPACE/logs
 
 # 检查是否有镜像
+echo "检查是否有镜像: $DOCKER_IMAGES"
 if [ -z "$DOCKER_IMAGES" ]; then
     echo -e "\033[33m  拉取 $DOCKER_CONTAINER_NAME:$DOCKER_CONTAINER_VERSION 镜像 \033[0m"
     docker pull $DOCKER_CONTAINER_NAME:$DOCKER_CONTAINER_VERSION
@@ -30,21 +31,27 @@ if [ -z "$DOCKER_IMAGES" ]; then
 fi
 
 # 判断是否有工作空间
+echo "判断是否有工作空间: $DOCKER_WROKSPACE"
 if [ ! -d "$DOCKER_WROKSPACE" ]; then
+    echo "创建工作空间"
     mkdir -p $NGINX_WORKSPACE_CONF $NGINX_WORKSPACE_FILES $NGINX_WORKSPACE_WEB $NGINX_WORKSPACE_LOGS
 fi
 
 # 判断是否有默认配置文件
+echo "判断是否有默认配置文件: $NGINX_WORKSPACE_CONF/nginx.conf"
 if [ ! -f "$NGINX_WORKSPACE_CONF/nginx.conf" ]; then
+    echo "初始化默认配置文件"
     # 写配置文件,默认的
     echo "IyDkuIDoiKzkuIDkuKrov5vnqIvotrPlpJ/kuoYs5aaC5p6c5pyJU1NM44CBZ3ppcOi/meS6m+avlOi+g+a2iOiAl0NQVeeahOW3peS9nO+8jOiAjOS4lOaYr+WkmuaguENQVeeahOivne+8jOWPr+S7peiuvuS4uuWSjENQVeeahOaVsOmHj+S4gOagtwojIHdvcmtlcl9wcm9jZXNzZXMgNDsKCmV2ZW50cyB7Cgl1c2UgZXBvbGw7Cgl3b3JrZXJfY29ubmVjdGlvbnMgMTAyNDsKCWFjY2VwdF9tdXRleCBvbjsKCW11bHRpX2FjY2VwdCBvbjsKfQoKaHR0cCB7CglzZXJ2ZXJfdG9rZW5zIG9mZjsKCWluY2x1ZGUgbWltZS50eXBlczsKCWRlZmF1bHRfdHlwZSBhcHBsaWNhdGlvbi9vY3RldC1zdHJlYW07CglhY2Nlc3NfbG9nIC92YXIvbG9nL25naW54L2FjY2Vzcy5sb2c7CgoJY2xpZW50X21heF9ib2R5X3NpemUgMTAyNE07CglzZW5kZmlsZSBvbjsKCgljbGllbnRfaGVhZGVyX2J1ZmZlcl9zaXplIDIwazsKCWxhcmdlX2NsaWVudF9oZWFkZXJfYnVmZmVycyA4IDQwazsKCWluY2x1ZGUgL2V0Yy9uZ2lueC9zZXJ2ZXJzLyouY29uZjsKfQo=" >$NGINX_WORKSPACE_CONF/nginx.conf
 fi
 
 # 判断是否有同名容器
 CHECK_CONTAINER_NAME=$(docker ps --format='{{.Names}}' | grep $DOCKER_CONTAINER_NAME)
+echo "判断是否有同名容器: $CHECK_CONTAINER_NAME"
 
 if [ -z "$CHECK_CONTAINER_NAME" ]; then
-    # 启动容器
+    echo "创建容器 $DOCKER_CONTAINER_NAME"
+    # 创建容器
     docker run \
         --name $DOCKER_CONTAINER_NAME \
         --network $DOCKER_NETWORK_BRIDGE_NAME \
