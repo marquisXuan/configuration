8d38e2378d7ed4458b16a473c973016bdb6d4c3f
叶云轩 <marquis_xuan@163.com>
2020-08-28 02:13:12 +0800

    添加了循环代码

--- linux/install/centos-7/docker/docker-spring-boot.sh
+++ linux/install/centos-7/docker/docker-spring-boot.sh
@@ -4,14 +4,21 @@ _path=$1
 _container_name=$2
 _env=$3
 
+DOCKER_CONTAINER_NAME=spring-boot-images
 DOCKER_IMAGES=$(docker images | grep spring-boot-images | awk '{print $1}')
 
 if [ -z "$DOCKER_IMAGES" ]; then
     __path=/srv/docker/java/base
     mkdir -p $__path && cd $__path
     echo "RlJPTSBvcGVuamRrOjgKRU5WIEpBVkFfT1BUSU9OUz0iIgpFTlRSWVBPSU5UIFsgInNoIiwgIi1jIiwgImphdmEgJEpBVkFfT1BUUyAtRGphdmEuc2VjdXJpdHkuZWdkPWZpbGU6L2Rldi8uL3VyYW5kb20gLWphciAvb3B0L2FwcC5qYXIiIF0K" | base64 -d >$__path/Dockerfile
-    DOCKER_IMAGES=spring-boot-images
-    docker build -t $DOCKER_IMAGES .
+
+    INNER_DOCKER_IMAGES=$(docker images | grep $DOCKER_CONTAINER_NAME | awk '{print $1}')
+
+    while (("$INNER_DOCKER_IMAGES" != "$DOCKER_CONTAINER_NAME")); do
+        docker pull openjdk:8
+        INNER_DOCKER_IMAGES=$(docker images | grep $DOCKER_CONTAINER_NAME | awk '{print $1}')
+    done
+    docker build -t $DOCKER_CONTAINER_NAME .
 fi
 
 # 基于 docker 的 spring-boot 容器
@@ -28,4 +35,5 @@ if [ -n "$_path" ]; then
         --restart=always \
         -d $DOCKER_IMAGES
 fi
+
 echo -e "\033[36m 无参时,只创建 SpringBoot 镜像 \033[0m"
