3a985467ac475dbc76240042dfaf372814a07174
叶云轩 <marquis_xuan@163.com>
2021-01-13 16:06:08 +0800

    change vimrc config with base64

--- linux/install/centos-7/docker/docker.sh
+++ linux/install/centos-7/docker/docker.sh
@@ -2,16 +2,16 @@
 
 groupDocker=$(cat /etc/group | grep docker | wc -l | awk '{print $0}')
 
-if test $groupDocker -eq 0 ; then
+if test $groupDocker -eq 0; then
   # docker组 todo 检查用户组是否存在
   groupadd docker -g 7654
 else
-  echo -e "\033[33m docker 组已存在 \033[33m"  
+  echo -e "\033[33m docker 组已存在 \033[33m"
 fi
 
 dockerInstalled=$(yum list installed | grep docker | wc -l | awk '{print $0}')
 
-if test $dockerInstalled -eq 0 ; then 
+if test $dockerInstalled -eq 0; then
   # 安装 docker 检查 docker 是否已经安装
   yum -y install docker
 else
@@ -20,7 +20,7 @@ fi
 
 # 修改 docker 软件源
 echo "ewoJInJlZ2lzdHJ5LW1pcnJvcnMiOgoJCVsKCQkJImh0dHA6Ly9mMmQ2Y2I0MC5tLmRhb2Nsb3VkLmlvIgoJCQksImh0dHA6Ly9odWItbWlycm9yLmMuMTYzLmNvbSIKCQkJLCJodHRwczovL2RvY2tlci5taXJyb3JzLnVzdGMuZWR1LmNuIgoJCQksImh0dHBzOi8vcmVnaXN0cnkuZG9ja2VyLWNuLmNvbSIKCQldCn0K" | base64 -d >/etc/docker/daemon.json
-      
+
 systemctl enable docker && systemctl start docker
 
 # #################### docker 容器安装 ####################
@@ -30,24 +30,24 @@ DOCKER_NETWORK_BRIDGE_NAME=nginx-network
 
 DOCKER_NETWORK_BRIDGE=$(docker network ls --filter=name=$DOCKER_NETWORK_BRIDGE_NAME | sed -n '2 ,1p' | awk '{print $2}')
 if [ -z "$DOCKER_NETWORK_BRIDGE" ]; then
-    echo -e "\033[33m  创建 docker 网桥:[$DOCKER_NETWORK_BRIDGE_NAME] \033[0m"
-    docker network create $DOCKER_NETWORK_BRIDGE_NAME
+  echo -e "\033[33m  创建 docker 网桥:[$DOCKER_NETWORK_BRIDGE_NAME] \033[0m"
+  docker network create $DOCKER_NETWORK_BRIDGE_NAME
 else
   echo -e "\033[33m 网桥:[$DOCKER_NETWORK_BRIDGE_NAME] 已存在\033[33m"
 fi
 DOCKER_NETWORK_BRIDGE=$(docker network ls --filter=name=$DOCKER_NETWORK_BRIDGE_NAME | sed -n '2 ,1p' | awk '{print $2}')
 if [ -z "$DOCKER_NETWORK_BRIDGE" ]; then
-    echo -e "\033[33m  网桥创建失败,程序即将退出 \033[0m"
-    exit
+  echo -e "\033[33m  网桥创建失败,程序即将退出 \033[0m"
+  exit
 fi
 
 # 网桥创建成功,可以创建容器了
 if [ -f "$PWD/docker-nginx.sh" ]; then
-    echo -e "\033[33m 创建 nginx 容器 \033[0m"
-    $($PWD/docker-nginx.sh)
+  echo -e "\033[33m 创建 nginx 容器 \033[0m"
+  $($PWD/docker-nginx.sh)
 fi
 
 if [ -f "$PWD/docker-mysql.sh" ]; then
-    echo -e "\033[33m 创建 mysql 容器 \033[0m"
-    $($PWD/docker-mysql.sh)
+  echo -e "\033[33m 创建 mysql 容器 \033[0m"
+  $($PWD/docker-mysql.sh)
 fi
--- linux/install/centos-7/system/install.sh
+++ linux/install/centos-7/system/install.sh
@@ -9,4 +9,4 @@ wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-
 yum clean all && yum makecache && yum update -y
 
 # 配置vim
-cat ../../../vimrc > ~/.vimrc
+echo 'OnNldCBudQo6bm9oCnNldCBpZ25vcmVjYXNlCnNldCBzaG9ydG1lc3M9YXRJCnN5bnRheCBvbgphdXRvY21kIEluc2VydExlYXZlICogc2Ugbm9jdWwKc2V0IHJ1bGVyCnNldCBzaG93Y21kCnNldCBmb2xkZW5hYmxlCnNldCBmb2xkbWV0aG9kPW1hbnVhbApzZXQgYXV0b3dyaXRlCnNldCBjdXJzb3JsaW5lCnNldCBjb25maXJtCnNldCBhdXRvaW5kZW50CnNldCBjaW5kZW50CnNldCB0YWJzdG9wPTQKc2V0IHNvZnR0YWJzdG9wPTQKc2V0IHNoaWZ0d2lkdGg9NApzZXQgbm9leHBhbmR0YWIKc2V0IHNtYXJ0dGFiCnNldCBobHNlYXJjaApzZXQgaW5jc2VhcmNoCnNldCBlbmM9dXRmLTgKc2V0IGZlbmNzPXV0Zi04LHVjcy1ib20sc2hpZnQtamlzLGdiMTgwMzAsZ2JrLGdiMjMxMixjcDkzNgpzZXQgbGFuZ21lbnU9emhfQ04uVVRGLTgKc2V0IHNob3dtYXRjaApzZXQgc21hcnRpbmRlbnQK' | base64 -d >~/.vimrc
